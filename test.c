ac2c155462ddb2027bbf321a183391686cebd033
D-TOKIOKA <solo223d@gmail.com>
2019-02-23 10:47:47 +0900

    2 thread. The result will differ for each program invocation.

--- test.c
+++ test.c
@@ -3,19 +3,35 @@
 #include <string.h>
 #include <pthread.h>
 
-void *test_func(void *param) {
+void *test_func1(void *param) {
   pthread_t thread_id;
+  int i;
   struct test_struct *ts = (struct test_struct *)param;
 
   thread_id = pthread_self();
   PRAC_PRINT("thread_ID=%ld\n", thread_id);
-  ts->a = 100;
-  print_struct(ts);
+  for(i = 0; i < 10000000; i++) {
+    ts->a -= 1;
+    ts->b -= 1;
+  }
+}
+
+void *test_func2(void *param) {
+  pthread_t thread_id;
+  int i;
+  struct test_struct *ts = (struct test_struct *)param;
+
+  thread_id = pthread_self();
+  PRAC_PRINT("thread_ID=%ld\n", thread_id);
+  for(i = 0; i < 10000000; i++) {
+    ts->a += 2;
+    ts->b += 2;
+  }
 }
 
 void main(){
-  pthread_t thread;
-  int pid;
+  pthread_t thread1;
+  pthread_t thread2;
   struct test_struct ts;
 
   printf("Hello World""\n");
@@ -32,6 +48,11 @@ void main(){
   strcpy(ts.ts1.buf, "REST\0");
   print_struct(&ts);
 
-  pthread_create(&thread, NULL, test_func, &ts);
-  pthread_join(thread, NULL);
+  pthread_create(&thread1, NULL, test_func1, &ts);
+  pthread_create(&thread2, NULL, test_func2, &ts);
+
+  pthread_join(thread1, NULL);
+  pthread_join(thread2, NULL);
+
+  print_struct(&ts);
 }
